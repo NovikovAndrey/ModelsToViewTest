27b0c81175907dc1a995aeb1984f6640217b533b
Андрей Новиков <novikov.andrey.v@hotmail.com>
2020-02-18 11:09:39 +0300

    Add controller

---
+++ ModelsToViewTest/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SingleModel.Model;
+
+namespace SingleModel.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            Game game = new Game
+            {
+                Name = "World of Warcraft ",
+                Platform = "PC",
+                Engine = "Unknown"
+            };
+            return View(game);
+        }
+    }
+}
