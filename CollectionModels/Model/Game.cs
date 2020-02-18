b5018f47f3c56f6cae58cdc4eb53f7a2dd97c48f
Андрей Новиков <novikov.andrey.v@hotmail.com>
2020-02-18 12:08:10 +0300

    Add model

---
+++ CollectionModels/Model/Game.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CollectionModels.Model
+{
+    public class Game
+    {
+        public string Name { get; set; }
+        public string Platform { get; set; }
+        public string Engine { get; set; }
+    }
+}
